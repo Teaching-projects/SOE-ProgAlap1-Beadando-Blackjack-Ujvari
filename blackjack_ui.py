681ade49373c74d7f8972c1590186cf6bcec5234
ujvarivera <ujvariveronika0320@gmail.com>
2020-12-24 14:02:46 +0100

    jav

--- blackjack_ui.py
+++ blackjack_ui.py
@@ -1,13 +1,16 @@
 import cards as CARDS
 
 def options():
+    print()
     print("Options: ")
     print("1: Hit")
     print("2: Stay")
     print("3: Current result to a json file and exit")
+    print()
 
 def getOption():
     option = int(input("Please, choose an option! "))
+    print()
     while str(option) not in "123": 
         print("Sorry, but there is no {}.option".format(option))
         option = int(input("Please, choose an option! "))
@@ -22,7 +25,11 @@ def main():
         options()
         option = getOption()
 
-        if option == 1 or option == 2:
+        if option == 3:
+            CARDS.save(player_cards,dealer_cards)
+            exit()
+
+        else:
             CARDS.getDealerCard(dealer_cards)
             print("Dealer has: {}, the sum of his cards is: {}".format(dealer_cards,CARDS.sumOfTheCards(dealer_cards)))
             if CARDS.ifBusted(dealer_cards):
@@ -34,30 +41,27 @@ def main():
                 print("The sum of your cards was: {}".format(CARDS.sumOfTheCards(player_cards)))
                 return
 
-        if option == 1:
-            CARDS.getPlayerCard(player_cards)
-            print("You have: {}, the sum of your cards is: {}".format(player_cards,CARDS.sumOfTheCards(player_cards)))
-            if CARDS.ifBusted(player_cards):
-                print("You have busted. Dealer won the game. The sum of the dealer cards is: {}".format(CARDS.sumOfTheCards(dealer_cards)))
-                return
-            if CARDS.ifBlackJack(player_cards):
-                print("You have BlackJack. You won the game! ")
-                print("The sum of the dealer cards was: {}".format(CARDS.sumOfTheCards(dealer_cards)))
-                return
+            if option == 1:
+                CARDS.getPlayerCard(player_cards)
+                print("You have: {}, the sum of your cards is: {}".format(player_cards,CARDS.sumOfTheCards(player_cards)))
+                if CARDS.ifBusted(player_cards):
+                    print("You have busted. Dealer won the game. The sum of the dealer cards is: {}".format(CARDS.sumOfTheCards(dealer_cards)))
+                    return
+                if CARDS.ifBlackJack(player_cards):
+                    print("You have BlackJack. You won the game! ")
+                    print("The sum of the dealer cards was: {}".format(CARDS.sumOfTheCards(dealer_cards)))
+                    return
 
-        if option == 2:
-            if CARDS.ifSumOfDealerCardsIsLarger(dealer_cards,player_cards):
-                print("Dealer won this game. The sum of his cards is: {}, yours is: {}".format(CARDS.sumOfTheCards(dealer_cards),CARDS.sumOfTheCards(player_cards)))
-                return
-            elif CARDS.ifDrawn(dealer_cards,player_cards):
-                print("Drawn")
-                return
-            else:
-                print("You won this game. The sum of your cards is: {}, his is: {}".format(CARDS.sumOfTheCards(player_cards),CARDS.sumOfTheCards(dealer_cards)))
-                return
+            if option == 2:
+                if CARDS.ifSumOfDealerCardsIsLarger(dealer_cards,player_cards):
+                    print("Dealer won this game. The sum of his cards is: {}, yours is: {}".format(CARDS.sumOfTheCards(dealer_cards),CARDS.sumOfTheCards(player_cards)))
+                    return
+                elif CARDS.ifDrawn(dealer_cards,player_cards):
+                    print("Drawn. No winner.")
+                    return
+                else:
+                    print("You won this game. The sum of your cards is: {}, his is: {}".format(CARDS.sumOfTheCards(player_cards),CARDS.sumOfTheCards(dealer_cards)))
+                    return
 
-        if option == 3:
-            CARDS.save(player_cards,dealer_cards)
-            exit()
 
     
