ce19f7f8d043c7ae2b1a3a332fb03dfaaced04c9
ujvarivera <ujvariveronika0320@gmail.com>
2021-01-01 20:20:03 +0100

    folyt

--- blackjack_ui.py
+++ blackjack_ui.py
@@ -1,4 +1,6 @@
 import cards as CARDS
+import json
+import os.path
 
 def options():
     print()
@@ -17,51 +19,91 @@ def getOption():
     return option
 
 def main():
-    player_cards = []
-    dealer_cards = []
+    file_exists = os.path.isfile(r'SOE-ProgAlap1-Beadando-Blackjack-Ujvari\blackjack.log')
+    if not file_exists:
+       with open(r'SOE-ProgAlap1-Beadando-Blackjack-Ujvari\blackjack.log', 'a+') as log:
+           pass
+    with open(r'SOE-ProgAlap1-Beadando-Blackjack-Ujvari\blackjack.log', 'r+') as log:
+       logs = log.readline()
+       if logs == '':
+          logs = []
+       else:
+          logs = logs.replace("'",'"')
+          logs = json.loads(logs)
+          
+       player_cards = []
+       dealer_cards = []
     
-    while True:
+       while True:
 
-        options()
-        option = getOption()
+           options()
+           option = getOption()
 
-        if option == 3:
-            CARDS.save(player_cards,dealer_cards)
-            exit()
+           if option == 3:
+               CARDS.save(player_cards,dealer_cards)
+               exit()
 
-        else:
-            CARDS.getDealerCard(dealer_cards)
-            print("Dealer has: {}, the sum of his cards is: {}".format(dealer_cards,CARDS.sumOfTheCards(dealer_cards)))
-            if CARDS.ifBusted(dealer_cards):
-                print("Dealer has busted, you win.")
-                print("The sum of your cards is: {}".format(CARDS.sumOfTheCards(player_cards)))
-                return
-            if CARDS.ifBlackJack(dealer_cards):
-                print("Dealer has BlackJack. Dealer won the game! ")
-                print("The sum of your cards was: {}".format(CARDS.sumOfTheCards(player_cards)))
-                return
+           else:
+               CARDS.getDealerCard(dealer_cards)
+               print("Dealer has: {}, the sum of his cards is: {}".format(dealer_cards,CARDS.sumOfTheCards(dealer_cards)))
+               if CARDS.ifBusted(dealer_cards):
+                   print("Dealer has busted, you win.")
+                   print("The sum of your cards is: {}".format(CARDS.sumOfTheCards(player_cards)))
+                   logs.append({"result":"win","reason":"Dealer has busted."})
+                   log.seek(0)
+                   log.write(str(logs))
+                   log.truncate()
+                   return
+               if CARDS.ifBlackJack(dealer_cards):
+                   print("Dealer has BlackJack. Dealer won the game! ")
+                   print("The sum of your cards was: {}".format(CARDS.sumOfTheCards(player_cards)))
+                   logs.append({"result":"lose","reason":"Dealer had BlackJack."})
+                   log.seek(0)
+                   log.write(str(logs))
+                   log.truncate()
+                   return
 
-            if option == 1:
-                CARDS.getPlayerCard(player_cards)
-                print("You have: {}, the sum of your cards is: {}".format(player_cards,CARDS.sumOfTheCards(player_cards)))
-                if CARDS.ifBusted(player_cards):
-                    print("You have busted. Dealer won the game. The sum of the dealer cards is: {}".format(CARDS.sumOfTheCards(dealer_cards)))
-                    return
-                if CARDS.ifBlackJack(player_cards):
-                    print("You have BlackJack. You won the game! ")
-                    print("The sum of the dealer cards was: {}".format(CARDS.sumOfTheCards(dealer_cards)))
-                    return
+               if option == 1:
+                   CARDS.getPlayerCard(player_cards)
+                   print("You have: {}, the sum of your cards is: {}".format(player_cards,CARDS.sumOfTheCards(player_cards)))
+                   if CARDS.ifBusted(player_cards):
+                       print("You have busted. Dealer won the game. The sum of the dealer cards is: {}".format(CARDS.sumOfTheCards(dealer_cards)))
+                       logs.append({"result":"lose","reason":"Player has busted."})
+                       log.seek(0)
+                       log.write(str(logs))
+                       log.truncate()
+                       return
+                   if CARDS.ifBlackJack(player_cards):
+                       print("You have BlackJack. You won the game! ")
+                       print("The sum of the dealer cards was: {}".format(CARDS.sumOfTheCards(dealer_cards)))
+                       logs.append({"result":"win","reason":"Player had BlackJack."})
+                       log.seek(0)
+                       log.write(str(logs))
+                       log.truncate()
+                       return
 
-            if option == 2:
-                if CARDS.ifSumOfDealerCardsIsLarger(dealer_cards,player_cards):
-                    print("Dealer won this game. The sum of his cards is: {}, yours is: {}".format(CARDS.sumOfTheCards(dealer_cards),CARDS.sumOfTheCards(player_cards)))
-                    return
-                elif CARDS.ifDrawn(dealer_cards,player_cards):
-                    print("Drawn. No winner.")
-                    return
-                else:
-                    print("You won this game. The sum of your cards is: {}, his is: {}".format(CARDS.sumOfTheCards(player_cards),CARDS.sumOfTheCards(dealer_cards)))
-                    return
+               if option == 2:
+                   if CARDS.ifSumOfDealerCardsIsLarger(dealer_cards,player_cards):
+                       print("Dealer won this game. The sum of his cards is: {}, yours is: {}".format(CARDS.sumOfTheCards(dealer_cards),CARDS.sumOfTheCards(player_cards)))
+                       logs.append({"result":"lose","reason":"Dealer was closer to 21."})
+                       log.seek(0)
+                       log.write(str(logs))
+                       log.truncate()
+                       return
+                   elif CARDS.ifDrawn(dealer_cards,player_cards):
+                       print("Drawn. No winner.")
+                       logs.append({"result":"draw","reason":"Both got the same value."})
+                       log.seek(0)
+                       log.write(str(logs))
+                       log.truncate()
+                       return
+                   else:
+                       print("You won this game. The sum of your cards is: {}, his is: {}".format(CARDS.sumOfTheCards(player_cards),CARDS.sumOfTheCards(dealer_cards)))
+                       logs.append({"result":"win","reason":"Player was closer to 21."})
+                       log.seek(0)
+                       log.write(str(logs))
+                       log.truncate()
+                       return
 
 
     
---
+++ stat.py
@@ -0,0 +1,37 @@
+import json
+
+with open("blackjack.log", "r") as read_file:
+    logs = read_file.readline()
+    logs = logs.replace("'",'"')
+    logs = json.loads(logs)
+
+lost = 0
+for i in range(len(logs)):
+    if logs[i]["result"] == "lose":
+        lost += 1
+
+won = 0
+for i in range(len(logs)):
+    if logs[i]["result"] == "win":
+        won += 1
+
+blackjack = 0
+for i in range(len(logs)):
+    if logs[i]["reason"] == "Player had BlackJack.":
+        blackjack += 1
+
+busted = 0
+for i in range(len(logs)):
+    if logs[i]["reason"] == "Player has busted.":
+        busted += 1
+
+drawn = 0
+for i in range(len(logs)):
+    if logs[i]["reason"] == "Both got the same value.":
+        drawn += 1
+
+
+print(logs)
+print("You lost {} times - you busted {} times".format(lost,busted))
+print("You won {} times - you had blackjack {} times".format(won,blackjack))
+print("You got the same value {} times.".format(drawn))
